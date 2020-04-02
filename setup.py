465cea64558b61c43c3cee0b188b60d360917e10
pierok13 <pierok@yahoo.fr>
2020-04-03 01:20:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,8 @@ setup(
     author_email='nicolas.graziano+py@gmail.com',
     packages= find_packages(),
     scripts=['bin/poll_isystem_mqtt.py', 'bin/dump_isystem.py'],
-    url='https://github.com/ngraziano/isystem-to-mqtt',
-    install_requires=['MinimalModbus', 'paho-mqtt'],
+    url='https://github.com/pierok13/isystem-to-mqtt',
+    install_requires=['pylibmodbus','MinimalModbus', 'paho-mqtt'],
     license='LICENSE',
     description='Request Isystem boiler and send value to mqtt',
     classifiers=['Programming Language :: Python :: 3']
